c1e94dd1f01adc032e7c0a64ff8787c2e2a15ab4
MiguelPalaciosC <miguel.palacios@uptc.edu.co>
2021-02-20 14:39:25 -0500

    Mensaje

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.set('port', 3000);
 
+app.set('/',(req,res)=>{
+  res.send('Hola mundo')
+})
+
 app.listen(app.get('port'), (req, res) => {
   console.log(`Server running on http://localhost:${app.get('port')}`);
 });
