a76a840053e4405ca952188ad720fa3f510e3343
hernanChain <hernanchain.dev@gmail.com>
2021-02-21 16:27:04 -0500

    Configure Routes Architecture

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
+const { cartRoutes, productsRoutes } = require('./routes');
 
-app.set('port', 3000);
+app.set('port', 4000);
+app.set('views', 'views');
+
+app.use('/', cartRoutes);
+app.use('/products', productsRoutes);
+
+app.use((req, res, next) => {
+  res.send('404: Page Not Found');
+});
 
 app.listen(app.get('port'), (req, res) => {
   console.log(`Server running on http://localhost:${app.get('port')}`);
