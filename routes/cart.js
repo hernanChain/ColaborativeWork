68b9bb698bb71006d84ee95fdb0861a2f70b55e0
hernanChain <hernanchain.dev@gmail.com>
2021-02-21 18:20:53 -0500

    Add dummy page for style them

--- routes/cart.js
+++ routes/cart.js
@@ -4,4 +4,11 @@ const path = require('path');
 router.get('/', (req, res) => {
   res.sendFile(path.join(rootDir, 'views', 'cart.html'));
 });
+router.get('/receipt', (req, res) => {
+  res.sendFile(path.join(rootDir, 'views', 'receipt.html'));
+});
+router.get('/addCart', (req, res) => {
+  res.sendFile(path.join(rootDir, 'views', 'addCart.html'));
+});
+
 module.exports = router;
