8d897e5b79c6672359883a8ec41d8108786c8f6d
MiguelPalaciosC <miguel.palacios@uptc.edu.co>
2021-02-26 21:59:06 -0500

    Add the views to ejs
    Views in ejs with images in the buttons

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
 const { cartRoutes, productsRoutes } = require('./routes');
 
 app.set('port', 4000);
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({ extended: false }));
